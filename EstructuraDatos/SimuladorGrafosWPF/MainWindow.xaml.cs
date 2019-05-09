db5a16f95de85113307e5c12ef8dd1d242ef631a
Duberney Cardona Cardona <duberney85@gmail.com>
2019-05-09 07:17:39 -0500

    Inicio diseño de interfaz

--- EstructuraDatos/SimuladorGrafosWPF/MainWindow.xaml.cs
+++ EstructuraDatos/SimuladorGrafosWPF/MainWindow.xaml.cs
@@ -1,37 +1,15 @@
-﻿using Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MahApps.Metro.Controls;
 
 namespace Client
 {
-	/// <summary>
-	/// Lógica de interacción para MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		Business business;
-		public MainWindow()
-		{
-			InitializeComponent();
-			business = new Business();
-		}
-
-		private void BtnEjercicio1_Click(object sender, RoutedEventArgs e)
-		{
-			//business.EjercicioPila();
-			business.Examen1Colas();
-		}
-	}
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
 }
