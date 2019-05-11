5c6ac608e016574e12fc920ce20931f3bbdd7a34
Duberney Cardona Cardona <duberney85@gmail.com>
2019-05-11 09:39:40 -0500

    Creacion de nuevos metodos en la clase grafo para hacer los recorridos

--- EstructuraDatos/Core/Grafo.cs
+++ EstructuraDatos/Core/Grafo.cs
@@ -4,23 +4,29 @@ using System.Drawing;
 
 namespace Core
 {
-	public class Grafo
-	{
-		private readonly int[,] _matrizAdyacencia;
-		private readonly int[] _indegree;
-		private readonly int _nodos;
+    public class Grafo
+    {
+        private readonly int[,] _matrizAdyacencia;
+        private readonly int[] _indegree;
+        private readonly int _nodos;
+        private List<Vertice> _listaNodos;
+
 
         /// <summary>
         /// Lista de nodos del grafo
         /// </summary>
-        public List<Vertice> Nodos;
+        public List<Vertice> ListaNodos
+        {
+            get { return _listaNodos; }
+            set { _listaNodos = value; }
+        }
 
         /// <summary>
         /// Crea un grafo para manejarlo con una lista de adyacencia
         /// </summary>
         public Grafo()
         {
-            Nodos = new List<Vertice>();
+            ListaNodos = new List<Vertice>();
         }
 
         /// <summary>
@@ -28,15 +34,15 @@ namespace Core
         /// </summary>
         /// <param name="totalNodos"></param>
 		public Grafo(int totalNodos)
-		{
-			_nodos = totalNodos;
+        {
+            _nodos = totalNodos;
 
-			// instancia de matriz de adyacencia 
-			_matrizAdyacencia = new int[_nodos, _nodos];
+            // instancia de matriz de adyacencia 
+            _matrizAdyacencia = new int[_nodos, _nodos];
 
-			// instancia del arreglo de indegree
-			_indegree = new int[_nodos];
-		}
+            // instancia del arreglo de indegree
+            _indegree = new int[_nodos];
+        }
 
         #region Metodos para trabajar el grafo con matriz de adyacencia
 
@@ -46,132 +52,132 @@ namespace Core
         /// <param name="nodoInicio">posicion inicial</param>
         /// <param name="nodoFin">posicion final</param>
         public void AdicionarArista(int nodoInicio, int nodoFin)
-		{
-			_matrizAdyacencia[nodoInicio, nodoFin] = 1;
-		}
+        {
+            _matrizAdyacencia[nodoInicio, nodoFin] = 1;
+        }
 
-		/// <summary>
-		/// Adiciona una arista entre dos nodos por medio de su peso
-		/// </summary>
-		/// <param name="nodoInicio"></param>
-		/// <param name="nodoFin"></param>
-		/// <param name="peso"></param>
-		public void AdicionarArista(int nodoInicio, int nodoFin, int peso)
-		{
-			_matrizAdyacencia[nodoInicio, nodoFin] = peso;
-		}
+        /// <summary>
+        /// Adiciona una arista entre dos nodos por medio de su peso
+        /// </summary>
+        /// <param name="nodoInicio"></param>
+        /// <param name="nodoFin"></param>
+        /// <param name="peso"></param>
+        public void AdicionarArista(int nodoInicio, int nodoFin, int peso)
+        {
+            _matrizAdyacencia[nodoInicio, nodoFin] = peso;
+        }
 
-		/// <summary>
-		/// Muestra la matriz de adyacencia en pantalla
-		/// </summary>
-		public void MostrarAdyacencia()
-		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine();
-			for (int n = 0; n < _nodos; n++)
-			{
-				Console.Write("   {0}", n);
-			}
+        /// <summary>
+        /// Muestra la matriz de adyacencia en pantalla
+        /// </summary>
+        public void MostrarAdyacencia()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            for (int n = 0; n < _nodos; n++)
+            {
+                Console.Write("   {0}", n);
+            }
 
-			// Se agrega nueva linea
-			Console.WriteLine();
+            // Se agrega nueva linea
+            Console.WriteLine();
 
-			for (int n = 0; n < _nodos; n++)
-			{
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.Write(n);
-				for (int m = 0; m < _nodos; m++)
-				{
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.Write("   {0}", _matrizAdyacencia[n, m]);
-				}
-				Console.WriteLine();
-			}
-		}
+            for (int n = 0; n < _nodos; n++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(n);
+                for (int m = 0; m < _nodos; m++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("   {0}", _matrizAdyacencia[n, m]);
+                }
+                Console.WriteLine();
+            }
+        }
 
-		/// <summary>
-		/// Muestra el peso almacenado que tiene una arista segun las coordenadas dadas de la matriz de adyacencia
-		/// </summary>
-		/// <param name="fila">Posicion de la fila</param>
-		/// <param name="columna">Posicion de la columna</param>
-		/// <returns></returns>
-		public int ObtenerAdyacencia(int fila, int columna)
-		{
-			return _matrizAdyacencia[fila, columna];
-		}
+        /// <summary>
+        /// Muestra el peso almacenado que tiene una arista segun las coordenadas dadas de la matriz de adyacencia
+        /// </summary>
+        /// <param name="fila">Posicion de la fila</param>
+        /// <param name="columna">Posicion de la columna</param>
+        /// <returns></returns>
+        public int ObtenerAdyacencia(int fila, int columna)
+        {
+            return _matrizAdyacencia[fila, columna];
+        }
 
-		/// <summary>
-		/// Calcula el indegree de los nodos 
-		/// </summary>
-		public void CalcularIndegree()
-		{
-			for (int n = 0; n < _nodos; n++)
-			{
-				for (int m = 0; m < _nodos; m++)
-				{
-					if (_matrizAdyacencia[m, n] == 1)
-					{
-						_indegree[n]++;
-					}
-				}
-			}
-		}
+        /// <summary>
+        /// Calcula el indegree de los nodos 
+        /// </summary>
+        public void CalcularIndegree()
+        {
+            for (int n = 0; n < _nodos; n++)
+            {
+                for (int m = 0; m < _nodos; m++)
+                {
+                    if (_matrizAdyacencia[m, n] == 1)
+                    {
+                        _indegree[n]++;
+                    }
+                }
+            }
+        }
 
-		/// <summary>
-		/// Muestra el Indegree de todos los nodos
-		/// </summary>
-		public void MostrarIndegree()
-		{
-			Console.ForegroundColor = ConsoleColor.White;
-			for (int n = 0; n < _nodos; n++)
-			{
-				Console.WriteLine("Nodo: {0}, {1}", n, _indegree[n]);
-			}
-		}
+        /// <summary>
+        /// Muestra el Indegree de todos los nodos
+        /// </summary>
+        public void MostrarIndegree()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int n = 0; n < _nodos; n++)
+            {
+                Console.WriteLine("Nodo: {0}, {1}", n, _indegree[n]);
+            }
+        }
 
-		/// <summary>
-		/// Encontrar nodo con indegree cero
-		/// </summary>
-		/// <returns>devuelve el index del nodo que tiene un indegree cero, si no encuentra ninguno devuelve -1</returns>
-		public int EncontrarIndegreeCero()
-		{
-			bool encontrado = false;
-			int n = 0;
-			for (n = 0; n < _nodos; n++)
-			{
-				if (_indegree[n] == 0)
-				{
-					encontrado = true;
-					break;
-				}
-			}
+        /// <summary>
+        /// Encontrar nodo con indegree cero
+        /// </summary>
+        /// <returns>devuelve el index del nodo que tiene un indegree cero, si no encuentra ninguno devuelve -1</returns>
+        public int EncontrarIndegreeCero()
+        {
+            bool encontrado = false;
+            int n = 0;
+            for (n = 0; n < _nodos; n++)
+            {
+                if (_indegree[n] == 0)
+                {
+                    encontrado = true;
+                    break;
+                }
+            }
 
-			if (encontrado)
-			{
-				return n;
-			}
-			else
-			{
-				return -1;
-			}
-		}
+            if (encontrado)
+            {
+                return n;
+            }
+            else
+            {
+                return -1;
+            }
+        }
 
-		/// <summary>
-		/// Disminuye un indigree de todos
-		/// </summary>
-		/// <param name="nodo"></param>
-		public void DecrementaIndigree(int nodo)
-		{
-			_indegree[nodo] = -1;
+        /// <summary>
+        /// Disminuye un indigree de todos
+        /// </summary>
+        /// <param name="nodo"></param>
+        public void DecrementaIndigree(int nodo)
+        {
+            _indegree[nodo] = -1;
 
-			for (int n = 0; n < _nodos; n++)
-			{
-				if (_matrizAdyacencia[nodo, n] == 1)
-				{
-					_indegree[n]--;
-				}
-			}
-		}
+            for (int n = 0; n < _nodos; n++)
+            {
+                if (_matrizAdyacencia[nodo, n] == 1)
+                {
+                    _indegree[n]--;
+                }
+            }
+        }
         #endregion Metodos para trabajar el grafo con matriz de adyacencia
 
         #region Metodos para trabajar el grafo con una lista de adyacencia
@@ -183,7 +189,7 @@ namespace Core
         public Vertice AgregarVertice(string nombre)
         {
             Vertice nuevoNodo = new Vertice(nombre);
-            Nodos.Add(nuevoNodo);
+            ListaNodos.Add(nuevoNodo);
             return nuevoNodo;
         }
 
@@ -193,7 +199,7 @@ namespace Core
         /// <param name="nuevoNodo"></param>
         public void AgregarVertice(Vertice nuevoNodo)
         {
-            Nodos.Add(nuevoNodo);
+            ListaNodos.Add(nuevoNodo);
         }
 
         /// <summary>
@@ -203,7 +209,7 @@ namespace Core
         /// <returns>Nodo encontrado</returns>
         public Vertice BuscarVertice(string valor)
         {
-            return Nodos.Find(x => x.Nombre == valor);
+            return ListaNodos.Find(x => x.Nombre == valor);
         }
 
         /// <summary>
@@ -217,12 +223,12 @@ namespace Core
         {
             Vertice nodoOrigen, nodoDestino;
 
-            if((nodoOrigen = Nodos.Find(x=>x.Nombre==nombreOrigen))==null)
+            if ((nodoOrigen = ListaNodos.Find(x => x.Nombre == nombreOrigen)) == null)
             {
                 throw new Exception(string.Format("El nodo {0} no existe dentro del grafo", nombreOrigen));
             }
 
-            if((nodoDestino = Nodos.Find(x=> x.Nombre == nombreDestino))==null)
+            if ((nodoDestino = ListaNodos.Find(x => x.Nombre == nombreDestino)) == null)
             {
                 throw new Exception(string.Format("El nodo {0} no existe dentro del grafo", nombreDestino));
             }
@@ -239,7 +245,7 @@ namespace Core
         /// <returns>True si adicionó, False si no se</returns>
         public bool AgregarArista(Vertice nodoOrigen, Vertice nodoDestion, int peso = 1)
         {
-            if(nodoOrigen.ListaAdyacencia.Find(x=>x.VerticeDestino == nodoOrigen)==null)
+            if (nodoOrigen.ListaAdyacencia.Find(x => x.VerticeDestino == nodoOrigen) == null)
             {
                 nodoOrigen.ListaAdyacencia.Add(new Arista(nodoDestion, peso));
                 return true;
@@ -255,13 +261,13 @@ namespace Core
         public void DibujarGrafo(Graphics g)
         {
             // Dibujar las aristas
-            foreach (Vertice nodo in Nodos)
+            foreach (Vertice nodo in ListaNodos)
             {
                 nodo.DibujarArista(g);
             }
 
             // Dibujar los vertices o nodos
-            foreach (Vertice nodo in Nodos)
+            foreach (Vertice nodo in ListaNodos)
             {
                 nodo.DibujarVertice(g);
             }
@@ -273,9 +279,9 @@ namespace Core
         /// <returns>Nodo seleccionado</returns>
         public Vertice DetectarPunto(Point posicionMouse)
         {
-            foreach (Vertice nodo in Nodos)
+            foreach (Vertice nodo in ListaNodos)
             {
-                if(nodo.DetectarPunto(posicionMouse))
+                if (nodo.DetectarPunto(posicionMouse))
                 {
                     return nodo;
                 }
@@ -290,7 +296,7 @@ namespace Core
         /// <param name="g">Grafico a dibujar</param>
         public void ReestablecerGrafo(Graphics g)
         {
-            foreach (Vertice nodo in Nodos)
+            foreach (Vertice nodo in ListaNodos)
             {
                 nodo.ColorVertice = Color.White;
                 nodo.ColorFuente = Color.Black;
@@ -302,6 +308,117 @@ namespace Core
             }
 
             DibujarGrafo(g);
+        }
+
+        /// <summary>
+        /// Colorea una arista
+        /// </summary>
+        /// <param name="nombreNodoOrigen">Nombre del nodo origen</param>
+        /// <param name="NombreNodoDestino">Nombre del nodo destino</param>
+        public void ColorArista(string nombreNodoOrigen, string NombreNodoDestino)
+        {
+            foreach (Vertice nodo in _listaNodos)
+            {
+                foreach (Arista arista in nodo.ListaAdyacencia)
+                {
+                    if (nodo.ListaAdyacencia != null &&
+                        nodo.Nombre == nombreNodoOrigen &&
+                        arista.VerticeDestino.Nombre == NombreNodoDestino)
+                    {
+                        arista.ColorArista = Color.Red;
+                        arista.GrosorFlecha = 4;
+                    }
+                }
+            }
+        }
+
+        public void Colorear(Vertice nodo)
+        {
+            nodo.ColorVertice = Color.AliceBlue;
+            nodo.ColorFuente = Color.Black;
+        }
+
+        public Vertice VerticeDistanciaMinima()
+        {
+            int min = int.MaxValue;
+            Vertice temp = null;
+            foreach (Vertice nodoOrigen in _listaNodos)
+            {
+                if (nodoOrigen.Visitado)
+                {
+                    foreach (Vertice nodoDestino in _listaNodos)
+                    {
+                        if (!nodoDestino.Visitado)
+                        {
+                            foreach (Arista arista in nodoOrigen.ListaAdyacencia)
+                            {
+                                if (arista.VerticeDestino == nodoDestino && min > arista.Peso)
+                                {
+                                    min = arista.Peso;
+                                    temp = nodoDestino;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return temp;
+        }
+
+        /// <summary>
+        /// Obtiene la poscion que ocupa un nodo de la lista de adyacencia
+        /// </summary>
+        /// <param name="nombreNodo"></param>
+        /// <returns></returns>
+        public int PosicionNodo(string nombreNodo)
+        {
+            for (int i = 0; i < _listaNodos.Count; i++)
+            {
+                if (string.Compare(_listaNodos[i].Nombre, nombreNodo) == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Re-dibuja las aristas que llegan a un nodo
+        /// </summary>
+        /// <param name="nodoDestino">Nodo al que se le van a redibujar las airtas </param>
+        public void DibujarAristasEntrantes(Vertice nodoDestino)
+        {
+            foreach (Vertice nodo in _listaNodos)
+            {
+                foreach (Arista arista in nodo.ListaAdyacencia)
+                {
+                    if (nodo.ListaAdyacencia != null && nodo != nodoDestino)
+                    {
+                        if (arista.VerticeDestino == nodoDestino)
+                        {
+                            arista.ColorArista = Color.Black;
+                            arista.GrosorFlecha = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Desmarca todos los nodos visitados
+        /// </summary>
+        public void DesmarcarVertices()
+        {
+            foreach (Vertice nodo in _listaNodos)
+            {
+                nodo.Visitado = false;
+                nodo.Padre = null;
+                nodo.DistanciaNodo = int.MaxValue;
+                nodo.PesoAsignado = false;
+            }
         }
 
         #endregion Metodos para trabajar el grafo con una lista de adyacencia
